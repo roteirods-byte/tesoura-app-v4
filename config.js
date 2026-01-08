e22646511f76778f8682b34fbcb4bde73026b55e
JORGE SOBRINHO <roteiro.ds@gmail.com>
2026-01-08 05:19:39 -0300

    Add files via upload

--- config.js
+++ config.js
@@ -1,1 +1,17 @@
+// app/js/config.js
+// Fonte única de configuração do V4
 
+window.TESOURA_CONFIG = {
+  // Jogadores (visual)
+  APP_PIN: "TESOURA2026",
+
+  // Diretoria (edita) + regra do CAIXA
+  // true = COM CAIXA | false = SEM CAIXA
+  DIRETORES: {
+    "1baidec": { caixa: true  },
+    "2thiago": { caixa: true  },
+    "3love":   { caixa: false },
+    "4le":     { caixa: false },
+    "5titi":   { caixa: false }
+  }
+};
